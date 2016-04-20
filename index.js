d4f5fba40b3c78641c7c8f9e7d8b1affe537f66d
Peleg Rosenthal <peleg@teacherspayteachers.com>
2016-04-20 16:15:21 -0400

    adding some code

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-window.onload = function () { console.log('loaded!') };
+window.onload = function () {
+  var formElement = document.getElementById('form');
+  var inputElement = document.getElementById('input');
+
+  formElement.onsubmit = function (event) {
+    event.preventDefault();
+    var cityName = inputElement.value;
+    getWeather(cityName);
+  };
+};
+
+function printWeather(event) {
+  var response = event.target.responseText;
+  document.write(response);
+}
+
+function getWeather(cityName) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather' +
+    '?appid=c2f2d170f6f6fc336058e9851edb828c' +
+    '&q=' + cityName +
+    '&units=imperial';
+
+  var xhrObject = new XMLHttpRequest();
+  xhrObject.open('GET', url, true);
+  xhrObject.onload = printWeather;
+  xhrObject.send();
+}
